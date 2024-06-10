f261454672ed61472012ae4e433e622bffc543d2
Guven Karanfil <guven.karanfil@smartface.io>
2024-06-10 22:37:46 +0300

    remove test

--- spec/appcircle_testing_distribution_action_spec.rb
+++
@@ -1,9 +0,0 @@
-describe Fastlane::Actions::AppcircleTestingDistributionAction do
-  describe '#run' do
-    it 'prints a message' do
-      expect(Fastlane::UI).to receive(:message).with("The appcircle_testing_distribution plugin is working!")
-
-      Fastlane::Actions::AppcircleTestingDistributionAction.run(nil)
-    end
-  end
-end
